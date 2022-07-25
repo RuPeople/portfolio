db9e780d257759a802855f4073a219d5db46cb57
rupeople <david.chamurliev@gmail.com>
2022-07-25 20:52:28 +1000

    directory restructuring

--- portfolio/server/routes/imageRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const imageController = require('../controllers/imageController')
-const checkRole = require('../middleware/checkRoleMiddleware')
-
-router.post('/', checkRole('ADMIN'), imageController.create)
-router.get('/', imageController.getAll)
-
-module.exports = router
